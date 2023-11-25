9ab2e53ffcc1cabe409986bfce786e48a78aceec
qyockey <qyockey@tutanota.de>
2023-11-25 00:34:02 -0800

    use linked list functions instead of node manipulation

--- vm_translator/linked_list.c
+++ vm_translator/linked_list.c
@@ -4,23 +4,49 @@
 #include "error_check.h"
 #include "linked_list.h"
 
-node *list_append(node *tail, void *data) {
-  node *new_tail = (node *) malloc(sizeof(node));
-  assert_malloc_success((void *) new_tail);
-  new_tail->data = data;
-  new_tail->next = NULL;
-  if (tail != NULL) {
-    tail->next = new_tail;
-  }
-  return new_tail;
+linked_list *new_linked_list() {
+    linked_list *list = (linked_list *) malloc(sizeof(linked_list));
+    list->head = NULL;
+    list->tail = NULL;
+    return list;
 }
 
-void list_dispose(node *head) {
-  node *next;
-  do {
-    next = head->next;
-    free(head);
-    head = next;
-  } while (next != NULL);
+void list_append(linked_list *list, void *data) {
+    list_node *new_tail = (list_node *) malloc(sizeof(list_node));
+    assert_malloc_success((void *) new_tail);
+    new_tail->data = data;
+    new_tail->next = NULL;
+    if (list->tail) {
+        list_node *old_tail = list->tail;
+        old_tail->next = new_tail;
+    }
+    if (!list->head) {
+        list->head = new_tail;
+    }
+    list->tail = new_tail;
+}
+
+void *list_get_data(const list_node *node) {
+    return node->data;
+}
+
+list_node *list_get_next(const list_node *node) {
+    return node->next;
+}
+
+void list_print(linked_list *list) {
+    for (list_node *node = list->head; node != NULL; node = node->next) {
+        printf("%d->", *((int *) node->data));
+    }
+    printf("\b\b  \n");
+}
+
+void list_dispose(linked_list *list) {
+    list_node *node = list->head;
+    while (node != NULL) {
+        list_node *next = node->next;
+        free(node);
+        node = next;
+    }
 }
 
--- vm_translator/linked_list.h
+++ vm_translator/linked_list.h
@@ -1,13 +1,22 @@
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
-typedef struct node {
-  void *data;
-  struct node *next;
-} node;
+typedef struct list_node {
+    void *data;
+    struct list_node *next;
+} list_node;
 
-node *list_append(node *tail, void *data);
-void list_dispose(node *head);
+typedef struct {
+    list_node *head;
+    list_node *tail;
+} linked_list;
+
+linked_list *new_linked_list();
+void list_append(linked_list *list, void *data);
+void *list_get_data(const list_node *node);
+list_node *list_get_next(const list_node *node);
+void list_print(linked_list *list);
+void list_dispose(linked_list *list);
 
 #endif
 
